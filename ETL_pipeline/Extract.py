95539f9d7a72c0315294cdc2ee19843c48688654
sumin1103k <165245124+sumin1103k@users.noreply.github.com>
2026-03-07 14:46:01 +0000

    Query Log를 DataFrame으로 불러오기. 다음은 Transform 설계

--- ETL_pipeline/Extract.py
+++ ETL_pipeline/Extract.py
@@ -1,13 +1,27 @@
 import ir_datasets
 import modin.pandas as pd
-from itertools import islice
 
-def Extract(sample_size=100):
-    # 데이터 로드
+
+def extract(yield_chunk_size=100_000):
+
+    # Load the raw dataset
     dataset = ir_datasets.load("aol-ia")
-    
-    # 쿼리 로그 데이터프레임으로 변환 (샘플 크기 제한)
-    # AolQlog 객체를 dict로 변환하여 DataFrame 생성
-    data = [item._asdict() for item in islice(dataset.qlogs_iter(), sample_size)]
-    df = pd.DataFrame(data)
-    return df
+    iterable = dataset.qlogs_iter()
+    rows = []
+
+    # Convert the iterable of query logs into a DataFrame in chunks
+    for q in iterable:
+        rows.append({
+            'user_id': q.user_id,
+            'query_id': q.query_id,
+            'query': q.query,
+            'query_orig': q.query_orig,
+            'time': q.time,
+        })
+
+        if len(rows) >= yield_chunk_size:
+            yield pd.DataFrame(rows)
+            rows.clear()
+
+    if rows:
+        yield pd.DataFrame(rows)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import ETL_pipeline.Extract as Extract
+from ETL_pipeline.Extract import extract
+import modin.pandas as pd
 
-# 샘플 크기를 조정하여 로드 시간 단축 (예: 100, 1000, 10000)
-sample_size = 100  # 필요에 따라 변경
-df = Extract.Extract(sample_size)
-print(f"Loaded {len(df)} records")
-print(df.head())
+if __name__ == "__main__":
+    data = extract()
+    print(data.__next__())
